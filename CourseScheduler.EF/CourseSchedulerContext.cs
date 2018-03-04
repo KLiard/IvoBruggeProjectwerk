34b681f1e1929f1ae23c65202cd3a971b6af523d
Kenny Liard <kenny.liard@wegroup.be>
2018-03-04 21:39:40 +0100

    Added Context and Domain project

---
+++ CourseScheduler.EF/CourseSchedulerContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CourseScheduler.EF
+{
+    public class CourseSchedulerContext
+        : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
